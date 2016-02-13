c0387eaba96f896999d3c338c9f2b7b3830aef8a
Martin Muñoz <im.mmun@gmail.com>
2016-02-13 16:24:42 -0500

    Include the vendored shim in vendor.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ module.exports = {
   name: 'ember-cli-reporter',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
 
     app.import(app.bowerDirectory + '/bugsnag/src/bugsnag.js');
+    app.import('vendor/shims/bugsnag.js');
   }
 };
